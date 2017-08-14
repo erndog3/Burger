dbd45a38091dece0d4f554603d763544590681f1
Ernesto R Espinoza <Ernesto@Ernestos-MacBook-Pro.local>
2017-08-13 19:25:58 -0700

    added updated server code for heroku

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ var methodOverride = require("method-override");
 var orm = require ("./config/orm.js");
 
 var app = express();
-var port = 3000;
+
+var PORT = process.env.PORT || 8080;
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
