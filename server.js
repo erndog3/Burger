5864a7664f30bd2ee6472b5d970cac18a10b7b96
Ernesto R Espinoza <Ernesto@Ernestos-MacBook-Pro.local>
2017-08-13 20:32:44 -0700

    make it better

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use('/', routes);
 
 
 //begins listener
-app.listen(port, function() { rem
-  console.log("Listening on PORT " + port);
+app.listen(PORT, function() {
+  console.log("Listening on PORT " + PORT);
 });
