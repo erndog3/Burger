b174924b4d2fefb4de6977dafa993f41b15359c7
Ernesto R Espinoza <Ernesto@Ernestos-MacBook-Pro.local>
2017-08-16 20:05:39 -0700

    added jawsDB

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,17 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user:"oet0zlivuugu71b9",
-    password: "qcymynrrdz70xx1o",
+var connection;
+if (process.env.JAWSDB_URL) {
+connections = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
     database: "burgers_db"
-});
+})
 
-connection.connect(function(err){
-    if(err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as ID#: " + connection.threadId);
 });
-module.exports = connection;
+connection.connect();
+module.exports = connection;s
